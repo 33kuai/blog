786c3365e349c289d2812a4cf30740feb8a28b0e
33kuai <33kuai@gmail.com>
2018-11-29 13:33:26 +0800

    elastic model

---
+++ blog/common/models/Book.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace common\models;
+
+class Book extends yii\elasticsearch\ActiveRecord
+{
+ 
+
+    /**
+     * @return array This model's mapping
+     */
+    public static function mapping()
+    {
+        return [
+            static::type() => [
+                'properties' => [
+                    'name'           => ['type' => 'string'],
+                    'author_name'    => ['type' => 'string'],
+                    'publisher_name' => ['type' => 'string'],
+                    'created_at'     => ['type' => 'long'],
+                    'updated_at'     => ['type' => 'long'],
+                    'status'         => ['type' => 'long'],
+                ]
+            ],
+        ];
+    }
+
+    /**
+     * Set (update) mappings for this model
+     */
+    public static function updateMapping()
+    {
+        $db = static::getDb();
+        $command = $db->createCommand();
+        $command->setMapping(static::index(), static::type(), static::mapping());
+    }
+
+    /**
+     * Create this model's index
+     */
+    public static function createIndex()
+    {
+        $db = static::getDb();
+        $command = $db->createCommand();
+        $command->createIndex(static::index(), [
+            'settings' => [ /* ... */ ],
+            'mappings' => static::mapping(),
+            //'warmers' => [ /* ... */ ],
+            //'aliases' => [ /* ... */ ],
+            //'creation_date' => '...'
+        ]);
+    }
+
+    /**
+     * Delete this model's index
+     */
+    public static function deleteIndex()
+    {
+        $db = static::getDb();
+        $command = $db->createCommand();
+        $command->deleteIndex(static::index(), static::type());
+    }
+}
